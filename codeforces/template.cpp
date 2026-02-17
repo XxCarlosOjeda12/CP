3186e7cff72fb22fd435787f2d47ab4fa45b19e4
Elias <carloswindow123@gmail.com>
2026-02-17 11:27:45 -0600

    template

--- codeforces/template.cpp
+++ codeforces/template.cpp
@@ -1,16 +1,27 @@
 #include "bits/stdc++.h"
 using namespace std;
- 
-#define ll long long
- 
+  
 #define       forn(i,n)              for(int i=0;i<n;i++)
 #define          all(v)              v.begin(), v.end()
 #define         rall(v)              v.rbegin(),v.rend()
- 
 #define            pb                push_back
 #define          sz(a)               (int)a.size()
 
- void solve(){
+typedef long long ll;
+typedef pair<int,int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+memset(memo, -1, sizeof memo); //inicializar tabla de memoizacion dp
+vi memo(n, -1); //metodo alternativo
+memset(arr, 0, sizeof arr); 
+
+const int INF = 1e9;
+const int LLINF = 4e18;
+const double EPS = 1e-9;
+
+ans = a ? b: c;
+
+void solve(){
     
  }
 
